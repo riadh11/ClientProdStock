74f07de1d6c0b4d554a68bf4e005b3b86f5eaf67
Elkamel <riadhelkamel1@hotmail.fr>
2019-02-16 11:57:05 +0100

    ajoute navbar et sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ClientProdStock';
+  showHideSidebar: boolean= false;
+  hidsidebar(showHideSidebar){
+    this.showHideSidebar=showHideSidebar;
+
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProduitComponent } from './produit/produit.component';
+import { ProduitMockService } from './produit/produit.mock.service';
+
+import { ContentComponent } from './content/content.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProduitComponent,
+    ContentComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ProduitMockService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
